8fee24451c709093a9c73006ef005801db3176d9
Cemal ÇİĞDEM <cemal_cigdem@outlook.com>
2025-12-22 17:24:16 +0300

    feat: add User entity and repository

---
+++ src/main/java/com/cemalcigdem/wallet/domain/User.java
@@ -0,0 +1,48 @@
+package com.cemalcigdem.wallet.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = @UniqueConstraint(name = "uk_users_email", columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false, length = 120)
+    private String fullName;
+
+
+    @Column(nullable = false, length = 180)
+    private String email;
+
+    protected User() {
+    }
+
+    public User(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
